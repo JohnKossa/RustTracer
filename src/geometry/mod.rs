5232bac571e429cf64d8e5bd564d88e87549c074
John Kossa <kg959ster@gmail.com>
2017-06-07 22:03:50 -0500

    Add files via upload

---
+++ src/geometry/mod.rs
@@ -0,0 +1,6 @@
+pub mod point;
+use self::point::*;
+pub mod ray;
+use self::ray::*;
+pub mod vector;
+use self::vector::*;
---
+++ src/geometry/ray.rs
@@ -0,0 +1,6 @@
+use geometry::point::Point;
+use geometry::vector::Vector;
+pub struct Ray{
+    pub start: Point,
+    pub direction: Vector
+}
