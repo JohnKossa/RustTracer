122ce34489ead2efe589fd54faf8a3ddfc6415d6
John Kossa <john.kossa@frogslayer.com>
2017-06-20 00:21:59 -0500

    Added additional operator overloading for Vector

--- src/geometry/vector.rs
+++ src/geometry/vector.rs
@@ -1,3 +1,5 @@
+use std::ops::{Add, Sub};
+
 #[derive(Debug, Copy, Clone)]
 pub struct Vector{
     pub x: f64,
@@ -14,19 +16,27 @@ impl Vector{
         Vector{x: x, y: y, z: z}
     }
 
-    pub fn add(self, other: Vector) -> Vector {
-        Vector{x: self.x-other.x, y: self.y-other.y, z: self.z-other.z}
-    }
-
-    pub fn subtract(self, other: Vector) -> Vector {
-        Vector{x: self.x-other.x, y: self.y-other.y, z: self.z-other.z}
-    }
-
     pub fn dot(self, other: Vector) -> f64 {
         (self.x * other.x) + (self.y * other.y) + (self.z * other.z)
     }
 
     pub fn scale(self, factor: f64) -> Vector {
         Vector{x: (self.x * factor), y: (self.y * factor), z: (self.z * factor)}
+    }
+}
+
+impl Add for Vector{
+    type Output = Vector;
+
+    fn add(self, other:Vector) -> Vector{
+        Vector{x:self.x+other.x, y: self.y + other.y, z: self.z + other.z}
+    }
+}
+
+impl Sub for Vector{
+    type Output = Vector;
+
+    fn sub(self, other: Vector) -> Vector{
+        Vector{x:self.x - other.x, y: self.y - other.y, z: self.z - other.z}
     }
 }
